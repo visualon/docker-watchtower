ac5d8c8c72844b3a299e849d880fc7b50c226b7e
Michael Kriese <michael.kriese@visualon.de>
2021-07-13 12:20:07 +0200

    test: use compose

--- windows/test.cmd
+++
@@ -1,7 +0,0 @@
-@echo off
-
-docker run --rm %IMAGE% -v \\.\pipe\docker_engine:\\.\pipe\docker_engine watchtower --no-pull --run-once
-if not "%ERRORLEVEL%" == "0" exit /B %ERRORLEVEL%
-
-docker-compose --file docker-compose.test.yml run sut
-if not "%ERRORLEVEL%" == "0" exit /B %ERRORLEVEL%
