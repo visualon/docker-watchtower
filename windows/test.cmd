c7001a6b15516063e427770d22c6516ff333a837
Michael Kriese <michael.kriese@visualon.de>
2021-07-13 12:11:20 +0200

    ci: update tests

--- windows/test.cmd
+++ windows/test.cmd
@@ -1,4 +1,7 @@
 @echo off
 
-docker run --rm %IMAGE% watchtower --help
+docker run --rm %IMAGE% -v \\.\pipe\docker_engine:\\.\pipe\docker_engine watchtower --no-pull --run-once
+if not "%ERRORLEVEL%" == "0" exit /B %ERRORLEVEL%
+
+docker-compose --file docker-compose.test.yml run sut
 if not "%ERRORLEVEL%" == "0" exit /B %ERRORLEVEL%
